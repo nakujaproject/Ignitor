2e656e348ee46a2e47315a3412d86edc51b7bc0b
Paul M <55873687+PMunyao@users.noreply.github.com>
2023-09-30 09:41:42 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include "mbed.h"
+#include <cmath>
+#include <vector>
+
+// Constants for Goertzel algorithm
+const float f_target = 100; // Target frequency [Hz]
+const float fs = 1000;      // Sampling frequency [Hz]
+const int N = 100;          // Number of samples for Goertzel algorithm
+
+// Goertzel algorithm variables
+float r, s, w;          // Goertzel coefficients
+float x_prev1 = 0;      // Previous output x(n-1)
+float x_prev2 = 0;      // Previous output x(n-2)
+float amplitude = 0;    // Amplitude of the target frequency
+float phase = 0;        // Phase of the target frequency
+
+// Data buffer for the moving window
+std::vector<float> data_window;
+
+// Simulated GPIO setup function (replace this with your actual setup logic)
+void setupGPIO(int pin, bool mode) {
+    std::cout << "Setting up GPIO pin " << pin << " in mode " << mode << std::endl;
+}
+
+// Simulated GPIO read function (replace this with your actual reading logic)
+bool readGPIO(int pin) {
+    std::cout << "Reading from GPIO pin " << pin << std::endl;
+    return true;  // Replace this with the actual reading logic
+}
+
+float readFlowSensor(int pin) {
+    setupGPIO(pin, false);  // Replace false with the actual mode for input
+
+    int flowRate = 0;
+
+    // Simulate periodic readings (for demonstration purposes)
+    for (int i = 0; i < 5; ++i) {  // Simulate 5 readings (adjust as needed)
+        // Assuming you have some delay to simulate periodic readings
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Simulated pulse detection (replace this with actual logic)
+        if (readGPIO(pin)) {
+            flowRate++;
+        }
+    }
+
+    return flowRate;
+}
+
+// Function to calculate the Goertzel coefficients
+void calcGoertzelCoefficients() {
+    int k = static_cast<int>(N * f_target / fs);
+    float theta = 2 * 3.14159 * k / N;
+    r = 2.0 * cos(theta);
+    s = sin(theta);
+    w = 2.0 * cos(2 * theta);
+}
+
+// Function to perform the Goertzel algorithm and obtain amplitude and phase data
+void performGoertzelAlgorithm(float x_curr) {
+    float x_temp = x_curr + r * x_prev1 - x_prev2;
+    x_prev2 = x_prev1;
+    x_prev1 = x_temp;
+
+    // Calculate magnitude squared (amplitude squared) from Goertzel output
+    float x_result = x_prev2 * x_prev2 + x_prev1 * x_prev1 - r * x_prev1 * x_prev2;
+    // Convert magnitude squared to amplitude and phase
+    amplitude = sqrt(x_result);
+    phase = atan2(x_prev1, x_prev2);
+}
+
+int main() {
+    // Initializing and configuring microcontroller peripherals and GPIO pins
+
+    calcGoertzelCoefficients();
+
+    while (true) {
+        // Read the input signal from the flow sensor
+        float input_signal = readFlowSensor(); // Replace readFlowSensor() with actual function to read from flow sensor
+
+        // Add the new data point to the data_window
+        data_window.push_back(input_signal);
+
+        // If the size of data_window exceeds N, remove the oldest data point
+        if (data_window.size() > N) {
+            data_window.erase(data_window.begin());
+        }
+
+        // Perform Goertzel algorithm on the data in the data_window
+        performGoertzelAlgorithm(input_signal);
+
+        // Fourier series approximation using Goertzel-derived amplitude and phase data
+        float t = 0; // Time variable for generating the Fourier series
+        float sum = 0; // Sum of the Fourier series
+        for (int i = 0; i < data_window.size(); i++) {
+            // Generate the ith harmonic component and add it to the sum
+            sum += amplitude * cos(2 * 3.14159 * f_target * t / fs + phase);
+            t += 1.0 / fs;
+        }
+
+        // Additional code for using the Fourier series approximation (sum)
+        // For example, you can use 'sum' to represent the approximated signal.
+        // ...
+
+        // Additional code for the main loop
+        // ...
+    }
+}
